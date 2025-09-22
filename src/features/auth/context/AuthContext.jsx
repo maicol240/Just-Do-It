eb5a2c58b7ead03aa65e38fb9dba5bc489a6d3db
Maicol <72515186+maicol240@users.noreply.github.com>
2025-09-22 00:46:13 -0400

    feat: add AuthContext for user authentication and loading state management

---
+++ src/features/auth/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+import { auth } from "../../../services/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { SyncLoader } from "react-spinners";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUSer] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // Listen for authentication state changes
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUSer(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "20%",
+        }}
+      >
+        <SyncLoader color="blue" />
+
+        <p style={{ textAlign: "center" }}>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "normalize.css";
 import "./index.css";
 import App from "./App.jsx";
+import { AuthProvider } from "./features/auth/context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
